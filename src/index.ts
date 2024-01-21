76e7850acbaedd86f4e446dd9ebbccffa8feb2f4
David Montesdeoca <backpackerhh@gmail.com>
2024-01-21 12:39:08 +0100

    Allow to sort linked lists

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
@@ -13,3 +14,16 @@ const stringSorter = new Sorter(charactersCollection);
 stringSorter.sort();
 
 console.log(stringSorter.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(-1);
+linkedList.add(0);
+linkedList.add(10);
+linkedList.add(-5);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+
+console.log(linkedListSorter.collection);
+console.log(linkedList.print());
