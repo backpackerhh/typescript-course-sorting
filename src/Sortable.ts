64f0b81b34e722f9941a1f30ef1e783d378c0d5e
David Montesdeoca <backpackerhh@gmail.com>
2024-01-21 13:09:59 +0100

    Change interface for abstract class to implement sorting
    That way, it's easier for the client using the sorting classes.

--- src/Sortable.ts
+++
@@ -1,5 +0,0 @@
-export interface Sortable {
-  length: number;
-  swap(leftIndex: number, rightIndex: number): void;
-  compare(leftIndex: number, rightIndex: number): boolean;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
-import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const numbersSorter = new Sorter(numbersCollection);
-numbersSorter.sort();
-
-console.log(numbersSorter.collection);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaba");
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
-
-console.log(stringSorter.collection);
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(1);
@@ -21,9 +16,6 @@ linkedList.add(-1);
 linkedList.add(0);
 linkedList.add(10);
 linkedList.add(-5);
+linkedList.sort();
 
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
-
-console.log(linkedListSorter.collection);
 console.log(linkedList.print());
